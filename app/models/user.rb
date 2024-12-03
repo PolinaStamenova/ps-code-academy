1064d09988ff9ad8a254ad341b299c6c3094e222
Polina Stamenova <polina.stamenova@gmail.com>
2024-12-03 18:23:25 +0200

    Add role & last_name to Users. Rename name to first_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,25 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # Enums
+  enum role: { student: 0, admin: 1 }
+
   # Associations
 
   # Validations
-  validates :name, presence: true
+  validates :first_name, :last_name,
+            presence: true,
+            length: { minimum: 2 },
+            format: { with: /\A[a-zA-Z]+\z/, message: 'only allows letters' }
+
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  # Methods
+  def admin?
+    role == 'admin'
+  end
+
+  def student?
+    role == 'student'
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_22_114740) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_03_144818) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name", default: "", null: false
+    t.string "first_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -23,7 +23,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_22_114740) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name", default: "", null: false
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
