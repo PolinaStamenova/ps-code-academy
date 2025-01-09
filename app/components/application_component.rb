2ce0dec23b975ed71303f821cb823f3ed417cb83
Polina Stamenova <polina.stamenova@gmail.com>
2025-01-09 16:32:52 +0200

    Inlcude ViewComponentsHelper in ApplicationComponent

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,6 +1,7 @@
 class ApplicationComponent < ViewComponent::Base
   include ActionView::Helpers::FormHelper
   include Turbo::FramesHelper
+  include ViewComponentsHelper
 
   # Solve undefined method 'rich_text_area_tag' for view_components
   # https://vchkhr.medium.com/how-to-solve-undefined-method-rich-text-area-tag-f0342b09d759
