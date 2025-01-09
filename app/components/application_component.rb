b704c4a73b4fda9e48a534408cddecb64f72f0df
Polina <79217155+PolinaStamenova@users.noreply.github.com>
2025-01-09 15:15:58 +0200

    Integrate ActionText (#26)
    * Integrate ActionText
    
    * Add the installation of libvips in CI

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,4 +1,8 @@
 class ApplicationComponent < ViewComponent::Base
   include ActionView::Helpers::FormHelper
   include Turbo::FramesHelper
+
+  # Solve undefined method 'rich_text_area_tag' for view_components
+  # https://vchkhr.medium.com/how-to-solve-undefined-method-rich-text-area-tag-f0342b09d759
+  delegate :rich_text_area_tag, to: :helpers
 end
