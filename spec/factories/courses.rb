f90a83f315d2e404420e70cb51de79c03d46ee35
Polina Stamenova <polina.stamenova@gmail.com>
2025-01-16 19:22:12 +0200

    Fix lints & install rubocop gems

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,4 +1,4 @@
-FactoryBot.define do
+FactoryBot.define do # rubocop:disable Metrics/BlockLength
   factory :course do
     name { Faker::Educator.course_name }
     description { Faker::Lorem.paragraph }
